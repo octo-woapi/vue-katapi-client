0808d9fe7780111bfe77e819549956b9b19456f1
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2019-04-24 09:04:24 +0200

    feat(services): add product service

---
+++ src/services/product-service.js
@@ -0,0 +1,15 @@
+export class ProductService {
+  constructor(api) {
+    this.api = api;
+  }
+
+  async listProducts() {
+    return [];
+  }
+
+  async getProduct() {
+    return null;
+  }
+}
+
+export default ProductService;
