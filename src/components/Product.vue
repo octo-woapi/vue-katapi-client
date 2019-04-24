7a870e9d92296d47610d3ae15001be57c9f1ff0d
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2019-04-24 09:03:55 +0200

    feat: add Product; update Home

---
+++ src/components/Product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="product">
+    <h4>{{ name }}</h4>
+    <p class="product__description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    name: String,
+    description: String,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <product name="Robe" description="Une tres belle robe" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Product from '@/components/Product.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Product,
   },
 };
 </script>
